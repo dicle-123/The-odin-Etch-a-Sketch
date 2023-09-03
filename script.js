fed3755526a9ba38cb57ce70741b18d3a3069b9c
dicle <dicledilek888@gmail.com>
2023-09-04 00:33:25 +0300

    change color of divs

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const gridContainer = document.getElementById("grid-container");
+const container = document.getElementById("append-container");
+
+const print = (event) => {
+  event.target.style.backgroundColor = "yellow";
+};
+
+const clean = (e) => {
+  e.target.style.backgroundColor = "";
+};
 
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    gridContainer.append(square);
+    const createDiv = document.createElement("div");
+    createDiv.classList.add("square");
+    container.appendChild(createDiv);
+    createDiv.addEventListener("mouseover", print);
+    createDiv.addEventListener("mouseleave", clean);
   }
 }
